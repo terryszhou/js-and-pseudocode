2cba096636e2042445355d4680308eafd9147fcd
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-05 17:33:19 -0500

    Solved Java Lab

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,6 +8,11 @@
 
 */
 
+let name = "Terry"
+let age = 27
+let address = "221B Baker Street"
+let greeting = "Hello, my name is " + name + ". I'm " + age + " years old and live at " + address + "."
+console.log(greeting)
 
 /*
 	Part 2: Create another variable called greeting2. This time, use the
@@ -20,6 +25,9 @@
 
 */
 
+let greeting2 = `Hello, my name is ${name}. I'm ${age} years old and live at ${address}.`
+console.log(greeting2)
+
 
 /* 
 	Part 3: "Code Testing." Let's check and see if we did everything correctly and that `greeting` returns the same value as `greeting2`.
@@ -27,6 +35,8 @@
 	If the return is false, correct your bugs above until `greeting` and `greeting2` are equivalent. 
 */
 
+let problemSolved = greeting === greeting2
+console.log(problemSolved)
 
 /*
 	Part 4: (No coding is needed for this part).
@@ -42,3 +52,12 @@
 	Bonus: What would the pseudocode look like if you also had to find the smallest number?
 */
 
+/*
+First we would put all twenty numbers into an array, and assign it to a variable. 
+Then we would let a second variable represent the largest number. 
+We would have to write a function that goes through the array item by item, and assigns a number from the array to the value of the second variable.
+The function would compare each item to the value of the second variable; if the item is higher, it will replace the second variable's value. If it's lower or equal, nothing happens.
+The function would go through the whole array and return the largest value.
+
+For the smallest number, we'd do the same thing, except we'd have the function reassign the second variable's value if it was less than the compared items, not greater.
+*/
